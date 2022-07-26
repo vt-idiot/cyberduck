b48d09a7d2f0b3af6579ed5ca0d45c85bf7aa933
AliveDevil <AliveDevil@users.noreply.github.com>
2022-07-26 13:00:10 +0200

    Use ProfilesProvider in ProfilesViewModel

--- core/native/refresh/src/main/csharp/ViewModels/Preferences/Pages/ProfilesViewModel.cs
+++ core/native/refresh/src/main/csharp/ViewModels/Preferences/Pages/ProfilesViewModel.cs
@@ -23,34 +23,15 @@ namespace Ch.Cyberduck.Core.Refresh.ViewModels.Preferences.Pages
     {
         private static Logger log = LogManager.getLogger(typeof(ProfilesViewModel).FullName);
 
-        private readonly Dictionary<ProfileDescription, DescribedProfile> installed = new();
-
-        public ProfilesViewModel(ProtocolFactory protocols, ProfileListObserver profileListObserver, Controller controller)
+        public ProfilesViewModel(ProtocolFactory protocols, ProfileListObserver profileListObserver, ProfilesProvider provider)
         {
-            BehaviorSubject<bool> initialized = new(false);
-            LoadProfiles = ReactiveCommand.CreateFromTask(async () =>
+            LoadProfiles = ReactiveCommand.Create(() =>
             {
-                // doesn't handle failures.
-                // On Cyberduck: Requires restarting Cyberduck, as the Preferences-window is cached.
-                // On Mountain Duck: everytime navigating to the view reruns LoadProfiles.
-                initialized.OnNext(true);
-                TaskCompletionSource<IEnumerable<ProfileDescription>> result = new();
-                var worker = new InternalProfilesSynchronizeWorker(protocols, result);
-                var action = new ProfilesWorkerBackgroundAction(controller, worker);
-                controller.background(action);
+                Busy = true;
+                return provider.Profiles;
+            });
 
-                try
-                {
-                    Busy = true;
-                    return await result.Task;
-                }
-                finally
-                {
-                    Busy = false;
-                }
-            }, initialized.Select(x => !x));
-
-            var profiles = LoadProfiles.ToObservableChangeSet()
+            var profiles = LoadProfiles.Switch()
                 .Filter(x => x.getProfile().isPresent())
                 .Filter(this.WhenAnyValue(v => v.FilterText)
                     .Throttle(TimeSpan.FromMilliseconds(500))
@@ -63,7 +44,7 @@ namespace Ch.Cyberduck.Core.Refresh.ViewModels.Preferences.Pages
                 .Sort(SortExpressionComparer<ProfileViewModel>.Ascending(x => x.Profile))
                 .ObserveOnDispatcher()
                 .Bind(Profiles)
-                .Subscribe();
+                .Subscribe(_ => Busy = false);
 
             profiles.Connect()
                 .WhenPropertyChanged(v => v.Installed, false)
@@ -92,20 +73,8 @@ namespace Ch.Cyberduck.Core.Refresh.ViewModels.Preferences.Pages
         [Reactive]
         public string FilterText { get; set; }
 
-        public ReactiveCommand<Unit, IEnumerable<ProfileDescription>> LoadProfiles { get; }
+        public ReactiveCommand<Unit, IObservableList<ProfileDescription>> LoadProfiles { get; }
 
         public BindingList<ProfileViewModel> Profiles { get; } = new();
-
-        private class InternalProfilesSynchronizeWorker : ProfilesSynchronizeWorker
-        {
-            private readonly TaskCompletionSource<IEnumerable<ProfileDescription>> completionSource;
-
-            public InternalProfilesSynchronizeWorker(ProtocolFactory protocolFactory, TaskCompletionSource<IEnumerable<ProfileDescription>> completionSource) : base(protocolFactory, ProfilesFinder.Visitor.Prefetch)
-            {
-                this.completionSource = completionSource;
-            }
-
-            public override void cleanup(object result) => completionSource.SetResult(Utils.ConvertFromJavaList<ProfileDescription>((java.util.Collection)result));
-        }
     }
 }
